e7d35fdcbfb56b84b760b99a03b156e1aa00eb30
jowellhong <98877013+jowellhong@users.noreply.github.com>
2022-04-06 10:17:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -30,6 +30,48 @@ for i in range (19):
 
     x = x+45
 
+y1 = 348
+x1 = 195
+i1 = 0
+
+for i1 in range (16):
+    cropped_image = img [y1:y1+85, x1:x1+49]
+    image = cv2.resize(cropped_image, (WIDTH, HEIGHT))
+    image_x = np.expand_dims(image, axis=0)
+    image_x = tf.keras.applications.vgg16.preprocess_input(image_x)
+    prediction = model.predict(image_x)
+    prediction = np.squeeze(prediction)
+
+    if prediction> 0.8:
+
+        cv2.rectangle(img, (x1, y1), (x1+49, y1+85),(0, 0, 255), 3)
+    else:
+
+        cv2.rectangle(img, (x1, y1), (x1+49, y1+85),(0, 255, 0), 3)
+        
+    x1 = x1+49
+
+y2 = 450
+x2 = 186
+i2 = 0
+
+for i2 in range (16):
+    cropped_image = img [y2:y2+85, x2:x2+50]
+    image = cv2.resize(cropped_image, (WIDTH, HEIGHT))
+    image_x = np.expand_dims(image, axis=0)
+    image_x = tf.keras.applications.vgg16.preprocess_input(image_x)
+    prediction = model.predict(image_x)
+    prediction = np.squeeze(prediction)
+
+    if prediction> 0.8:
+
+        cv2.rectangle(img, (x2, y2), (x2+50, y2+85),(0, 0, 255), 3)
+    else:
+
+        cv2.rectangle(img, (x2, y2), (x2+50, y2+85),(0, 255, 0), 3)
+        
+    x2 = x2+50
+    
 cv2.imshow('parking lot', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
